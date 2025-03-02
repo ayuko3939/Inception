cae83640ee0d3f841aa8e247b3771853095c1e06
Yoka Hasegawa <yoka309hase@gmail.com>
2025-03-02 11:38:19 +0900

    fit

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -6,7 +6,7 @@ cd /etc/nginx/ssl
 openssl genrsa -out inception.key 2048
 
 openssl req -new -key inception.key -out inception.csr \
-    -subj "/C=JP/ST=Tokyo/L=Sinjuku/O=42Tokyo/CN=yohasega.42.fr"
+    -subj "/C=JP/ST=Tokyo/L=Shinjuku/O=42Tokyo/CN=yohasega.42.fr"
 
 openssl x509 -req -days 365 -in inception.csr -signkey inception.key -out inception.crt
 
